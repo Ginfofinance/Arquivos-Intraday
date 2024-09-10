76f335284d266371b9cfd9e31f4f52ce7d845d14
Ginfofinance <85579477+Ginfofinance@users.noreply.github.com>
2024-09-10 09:59:11 -0300

    Update edit_databse.py

--- edit_databse.py
+++ edit_databse.py
@@ -1,17 +1,33 @@
 import sqlite3
 
 # Caminho para o banco de dados
-db_path = r"E:\progamação\vs code\Bovdb\tabela_dates\Database_define.db"
+db_path = r'C:\bovdb\volume\price_volume copy.db'
 
 # Conectar ao banco de dados
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
-# Atualizar as colunas 'volume', 'business' e 'amount_stock' para NULL
-cursor.execute("UPDATE price5 SET volume = NULL, business = NULL, amount_stock = NULL")
+# Excluir a tabela 'price5' se existir
+cursor.execute('DROP TABLE IF EXISTS price5')
 
-# Salvar as mudanças e fechar a conexão
+# Criar a tabela 'price5' com a nova estrutura
+create_table_query = '''
+CREATE TABLE price5 (
+    id_ticker INTEGER,
+    date DATE,
+    time TIME,
+    volume DECIMAL,
+    business INTEGER,
+    amount_stock INTEGER,
+    PRIMARY KEY (id_ticker, date, time),
+    FOREIGN KEY (id_ticker) REFERENCES Ticker(id_ticker)
+)
+'''
+
+cursor.execute(create_table_query)
+
+# Confirmar as alterações e fechar a conexão
 conn.commit()
 conn.close()
 
-print("As informações das colunas 'volume', 'business' e 'amount_stock' foram excluídas.")
+print("Tabela 'price5' recriada com sucesso.")
